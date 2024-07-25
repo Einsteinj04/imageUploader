88f9ba82118b534128867ce20361085d36449580
Einstein-J <jilchrisdascientist@gmail.com>
2024-07-25 05:42:07 +0100

    Arranged file structure

---
+++ myimguploaderproj/frontend/imguploaderfrontend/pages/loader.js
@@ -0,0 +1,7 @@
+export default function Loader(){
+    return(
+        <div>
+            <h1 className="underline text-3xl">Loading...</h1>
+        </div>
+    )
+}
--- myimguploaderproj/frontend/imguploaderfrontend/pages/upload.js
+++ myimguploaderproj/frontend/imguploaderfrontend/pages/upload.js
@@ -1,11 +1,13 @@
 import axios from "axios";
-import {useState, useEffect} from 'react'
-
+import {useState} from 'react'
+import Loader from './loader'
+import '../src/app/globals.css'
 
 export default function Upload(){
    const [message, setMessage] = useState('')
    const [selectedFile, setSelectedFile] = useState(null)
    const [imgPath, setImgPath] = useState('')
+   const [loading, setLoading] = useState(false)
 
    const handleFileInput = (e) => {
         setSelectedFile(e.target.files[0])
@@ -14,6 +16,7 @@ export default function Upload(){
     e.preventDefault()
 
     if (selectedFile){
+        setLoading(true)
       const formData = new FormData();
       formData.append('upload',selectedFile);
       const response = await axios.post('http://127.0.0.1:8000/api/upload/',formData,{
@@ -21,6 +24,7 @@ export default function Upload(){
           'Content-Type': 'multipart/form-data'
         }
       })
+      setLoading(false)
       setMessage('Uploaded Sucessfully')
       setImgPath(response.data.upload)
     }
@@ -30,15 +34,16 @@ export default function Upload(){
   }
 
    return(
-    <div>
-      <form onSubmit = {handleSubmit}>
-        <h1>Upload your images here</h1>
+    <div className="min-h-screen bg-white">
+        {loading?<Loader/>:
+        <form onSubmit = {handleSubmit} className="">
+        <h1 className="underline text-3xl text-red-700">Upload your images here</h1>
         <input type="file" onChange = {handleFileInput}/><br/><br/>
         <img src = {imgPath?`http://localhost:8000${imgPath}`: ''}
          style={{ width: '200px', height: 'auto', margin: '10px' }}/>
         <button>Upload</button>
         <p>{message}</p>
-      </form>
+      </form>}
     </div>
    )
 }
@@ -97,62 +102,3 @@ export default function Upload(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useState } from 'react';
-// import axios from 'axios';
-
-// export default function Upload() {
-//   const [selectedFile, setSelectedFile] = useState(null);
-//   const [message, setMessage] = useState('');
-
-//   const handleFileChange = (event) => {
-//     setSelectedFile(event.target.files[0]);
-//   };
-
-//   const handleUpload = async () => {
-//     const formData = new FormData();
-//     formData.append('upload', selectedFile);
-
-//     try {
-//       const response = await axios.post('http://127.0.0.1:8000/api/upload/', formData, {
-//         headers: {
-//           'Content-Type': 'multipart/form-data',
-//         },
-//       });
-//       setMessage('Image uploaded successfully');
-//     } catch (error) {
-//       setMessage('Error uploading image');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>Upload Image</h1>
-//       <input type="file" onChange={handleFileChange} />
-//       <button onClick={handleUpload}>Upload</button>
-//       <p>{message}</p>
-//     </div>
-//   );
-// }
-   
